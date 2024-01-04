fc9f0ca7ca933e8db3bde1089b70d4a4e9282714
ishwersharma13 <ishwersharma@gmail.com>
2024-01-04 11:53:23 +0530

    add

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -1,8 +1,8 @@
 import RESTAPIAdapter from '@ember-data/adapter/rest';
 
 export default class ApplicationAdapter extends RESTAPIAdapter {
-  namespace = 'api';
-  buildURL(...args) {
-    return `${super.buildURL(...args)}.json`;
-  }
+  namespace = 'api'
+buildURL(...args){
+  return `${super.buildURL(...args)}.json`
+}
 }
