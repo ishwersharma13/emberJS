881141950da417ec7d43cf90d208fca0b9a65527
ishwersharma13 <ishwersharma@gmail.com>
2024-01-04 18:28:52 +0530

    product page corrected

--- app/controllers/cart.js
+++ app/controllers/cart.js
@@ -1,6 +1,8 @@
+// cart.js
 import Controller from '@ember/controller';
 import { inject as service } from '@ember/service';
 import { action } from '@ember/object';
+
 export default class CartController extends Controller {
   @service('shopping-cart') cart;
 
@@ -9,20 +11,44 @@ export default class CartController extends Controller {
       return acc + item.price * item.count;
     }, 0);
   }
+
   get tax() {
     return 0.05 * this.subtotal;
   }
+
   get total() {
     return this.subtotal + this.tax;
   }
 
   @action
   updateItemCount(item, event) {
-    const count = event.target.value;
-    if (count >= 0) {
+    const count = parseInt(event.target.value, 10);
+
+    if (!isNaN(count) && count >= 0) {
       item.count = count;
     } else {
+      // Handle invalid input, reset count to 0 or any other logic
       item.count = 0;
+    }
+
+    // If count is less than 1, remove the item from the cart
+    if (item.count < 1) {
+      this.cart.removeItem(item);
+    }
+  }
+
+  @action
+  incrementItemCount(item) {
+    item.count += 1;
+  }
+
+  @action
+  decrementItemCount(item) {
+    if (item.count > 1) {
+      item.count -= 1;
+    } else {
+      // Remove the item from the cart
+      this.cart.removeItem(item);
     }
   }
 }
