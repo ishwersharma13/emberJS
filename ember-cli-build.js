61d866f171c2b03f9e89f3394027a75c94a3b417
ishwersharma13 <ishwersharma@gmail.com>
2024-01-04 12:11:11 +0530

    pics added

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -7,6 +7,9 @@ module.exports = function (defaults) {
     sassOptions: {
       extension: 'scss',
     },
+    fingerprint : {
+      exclude : ["assets/images"]
+    }
   });
 
   return app.toTree();
