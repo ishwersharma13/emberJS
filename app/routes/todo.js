8a6e8868025700152881a828086c285ed7129595
ishwersharma13 <ishwersharma@gmail.com>
2024-01-05 10:17:32 +0530

    delete button

--- app/routes/todo.js
+++ app/routes/todo.js
@@ -2,6 +2,6 @@ import Route from '@ember/routing/route';
 
 export default class TodoRoute extends Route {
   model() {
-    return ['Revise JS', 'Learn Ember', 'Build Project'];
+    return ['Revise JS', 'Learn Ember', 'Build Project','Todo App','E-Commerce App'];
   }
 }
