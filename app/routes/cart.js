3e7e4354355e16fc353df4d9cbc7d55ecc9f93cc
ishwersharma13 <ishwersharma@gmail.com>
2024-01-05 09:49:29 +0530

    todo added

--- app/routes/cart.js
+++ app/routes/cart.js
@@ -1,8 +1,5 @@
 import Route from '@ember/routing/route';
 
 export default class CartRoute extends Route {
-  model() {
-    const items = [{ price: 10 }, { price: 20 }];
-    return items;
-  }
+
 }
